d8638f6fb675e5a17da42d7dc171ed509cde6e82
Li Chen <philosopherchenli@yahoo.com>
2014-06-07 13:39:59 -0400

    add redirect_to root path

--- app/controllers/facebook_sessions_controller.rb
+++ app/controllers/facebook_sessions_controller.rb
@@ -2,9 +2,11 @@ class FacebookSessionsController < ApplicationController
   def create
     facebook_user = FacebookUser.from_omniauth(env["omniauth.auth"])
     facebook_session[:user_id] = user.id
+    redirect_to root_path
   end
 
   def destroy
     facebook_session[:user_id] = nil
+    redirect_to root_path
   end
 end
